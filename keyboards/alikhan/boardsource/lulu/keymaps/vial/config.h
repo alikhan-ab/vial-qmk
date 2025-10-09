ba0b8b7bd70530e80ba6f3375d6fcb1bdcb7bed3
Alikhan Abutalipov <13288276+alikhan-ab@users.noreply.github.com>
2025-10-10 00:27:36 +0500

    Add id and set layer count to 6

--- keyboards/alikhan/boardsource/lulu/keymaps/vial/config.h
+++ keyboards/alikhan/boardsource/lulu/keymaps/vial/config.h
@@ -2,9 +2,11 @@
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0xAB, 0x22, 0x68, 0x8B, 0x59, 0xD0, 0x81, 0x11}
+#define VIAL_KEYBOARD_UID {0x7F, 0xE0, 0x25, 0xA8, 0xDE, 0xB6, 0xAA, 0x63}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 #if defined(__AVR_ATmega32U4__)
     #undef ENABLE_RGB_MATRIX_BAND_SAT
